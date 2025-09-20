3db5ecf318be7075da38f77cfded6ade3b291764
Sherpad Ndabambi <sgndabambi@gmail.com>
2025-09-20 18:01:47 +0200

    Update warning storage in warnings command

--- commands/warnings.js
+++ commands/warnings.js
@@ -1,19 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-
-const warningsFilePath = path.join(__dirname, '../data/warnings.json');
-
-function loadWarnings() {
-    if (!fs.existsSync(warningsFilePath)) {
-        fs.writeFileSync(warningsFilePath, JSON.stringify({}), 'utf8');
-    }
-    const data = fs.readFileSync(warningsFilePath, 'utf8');
-    return JSON.parse(data);
-}
+const { getWarningCount } = require('../lib/index');
 
 async function warningsCommand(sock, chatId, mentionedJidList) {
-    const warnings = loadWarnings();
-
     if (!mentionedJidList || mentionedJidList.length === 0) {
         await sock.sendMessage(chatId, { text: 'Please mention a user to check warnings.' });
         return;
@@ -21,12 +10,12 @@ async function warningsCommand(sock, chatId, mentionedJidList) {
 
     const userToCheck = mentionedJidList[0];
 
-    // FIX: look up the per-chat structure
-    const warningCount = (warnings[chatId] && warnings[chatId][userToCheck]) || 0;
+    const warningCount = await getWarningCount(chatId, userToCheck);
 
     // mention the user in the reply
     await sock.sendMessage(chatId, { 
-        text: `User has ${warningCount} warning(s).`,
+        text: `@${userToCheck.split('@')[0]} has ${warningCount} warning(s).`,
+        mentions: [userToCheck]
     });
 }
 
